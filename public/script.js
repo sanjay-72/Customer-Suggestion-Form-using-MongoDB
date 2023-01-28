b969a2f14f8026b368f0b898480e8eaccc9705c5
Sanjay Kumar Konakandla <sanjaykumarkonakandla@gmail.com>
2023-01-28 18:53:40 +0530

    added effects

---
+++ public/script.js
@@ -0,0 +1,28 @@
+document.querySelector(
+    "body").style.visibility = "hidden";
+document.getElementById("jsWarning").style.display = "none";
+document.querySelector(
+    "#Loader").style.visibility = "visible";
+document.body.style.backgroundColor = "white";
+
+setTimeout(function () {
+    document.querySelector(
+        "#Loader").style.display = "none";
+    document.querySelector(
+        "body").style.visibility = "visible";
+    document.getElementById("fullNameInput").focus();
+    document.body.style.backgroundColor = 'rgb(' + 206 + ',' + 249 + ',' + 240 + ')';
+}, 6000);
+
+VanillaTilt.init(document.querySelectorAll(".tilt"), {
+    max: 15,
+    speed: 400
+});
+
+document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === 'visible') {
+        document.title = "Suggestion Form";
+    } else {
+        document.title = "Please give your suggestion.";
+    }
+});
